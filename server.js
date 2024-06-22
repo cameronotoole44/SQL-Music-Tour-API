35524afecdaac7f4ada9c42969e4a52bd838154c
cameronotoole44 <cameronotoole404@gmail.com>
2024-06-22 11:10:38 -0400

    connected

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
-// DEPENDENCIES
-const express = require('express')
-const app = express()
+// DEPENDENCIES //
+const express = require('express');
+const app = express();
+const { Sequelize } = require('sequelize');
 
-// CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// CONFIGURATION / MIDDLEWARE //
+require('dotenv').config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// ROOT
+// SEQUELIZE CONNECTION DIDNT WORK //
+// const sequelize = new Sequelize(process.env.PG_URI);
+
+// try {
+//     sequelize.authenticate()
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+// } catch (err) {
+//     console.log(`Unable to connect to PG: ${err}`)
+// };
+
+// OTHER OPTION FOR SEQ CONNECTION THIS WORKS OTHER DIDNT // 
+
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'my_password'
+});
+
+// ROOT //
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
     })
-})
+});
 
-// LISTEN
+// LISTEN //
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+});
